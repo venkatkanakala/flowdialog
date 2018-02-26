27cf6e03abfa044d1b4b09554bb341dfb2c187b1
Rao Kanakala <venkat.kanakala@gmail.com>
2018-02-26 16:23:13 -0600

    Updates to response text

--- index.js
+++ index.js
@@ -115,12 +115,14 @@ server.post('/get-movie-details', (request, response) => {
             responseFromAPI.on("data", responeData => {
                 logger.info('Response Data : ' + responeData);
 
-                let body = JSON.parse(responeData);
+                let movie = JSON.parse(responeData);
+                let dataToSend = movieToSearch === 'The Godfather' ? 'I don\'t have the required info on that. Here\'s some info on \'The Godfather\' instead.\n' : '';
+                dataToSend += movie.Title + ' is a ' + movie.Actors + ' starer ' + movie.Genre + ' movie, released in ' + movie.Year + '. It was directed by ' + movie.Director;
 
                 //Send Respond back to service
                 return response.json({
-                    speech: body,
-                    displayText: body,
+                    speech: dataToSend,
+                    displayText: dataToSend,
                     source: 'get-movie-details'
                 });
 
